f8c828008b1d1aa124cb0baa96d6f51bc1a74317
timvantongeren <tvtongeren12@gmail.com>
2023-12-20 17:55:55 +0100

    Is it ugly? Yes. Does it work? Yes

--- 2023/7/solution_part2.py
+++ 2023/7/solution_part2.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from itertools import product
 
 JOKER = -1
 
@@ -7,6 +8,9 @@ def get_cards(line: str) -> list[int]:
     cards_as_strings = [c for c in line]
     strength_map = {"A": 14, "K": 13, "Q": 12, "J": JOKER, "T": 10}
     return [int(strength_map.get(c, c)) for c in cards_as_strings]
+
+
+ALL_CARDS_BESIDES_JOKER = [14, 13, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2]
 
 
 def is_five_of_a_kind(hand: list[int]) -> bool:
@@ -87,7 +91,39 @@ assert is_high_card([1, 1, 8, 2, 2]) == False
 assert is_high_card([1, 9, 8, 3, 2]) == True
 
 
+def generate_all_permutations(hand: list[int]) -> list[list[int]]:
+    joker_count = hand.count(JOKER)
+    possible_joker_usage = [
+        list(t) for t in product(ALL_CARDS_BESIDES_JOKER, repeat=joker_count)
+    ]
+    # This is some magic to keep the same order
+    return [
+        [c if c != JOKER else jokers.pop() for c in hand]
+        for jokers in possible_joker_usage
+    ]
+
+
+assert generate_all_permutations([1, 1, 1, JOKER, 1]) == [
+    [1, 1, 1, 14, 1],
+    [1, 1, 1, 13, 1],
+    [1, 1, 1, 12, 1],
+    [1, 1, 1, 10, 1],
+    [1, 1, 1, 9, 1],
+    [1, 1, 1, 8, 1],
+    [1, 1, 1, 7, 1],
+    [1, 1, 1, 6, 1],
+    [1, 1, 1, 5, 1],
+    [1, 1, 1, 4, 1],
+    [1, 1, 1, 3, 1],
+    [1, 1, 1, 2, 1],
+]
+
+assert [2, 2, 2, 2, 2] in generate_all_permutations([2, 2, 2, JOKER, JOKER])
+
+
 def hand_a_stronger_than_hand_b(hand_a: list[int], hand_b: list[int]) -> bool:
+    hand_a_permutations: list[list[int]] = []
+    hand_b_permutations: list[list[int]] = []
     for is_strongest_hand in [
         is_five_of_a_kind,
         is_four_of_a_kind,
@@ -97,8 +133,25 @@ def hand_a_stronger_than_hand_b(hand_a: list[int], hand_b: list[int]) -> bool:
         is_one_pair,
         is_high_card,
     ]:
-        hand_a_is_this_hand = is_strongest_hand(hand_a)
-        hand_b_is_this_hand = is_strongest_hand(hand_b)
+        if hand_a.count(JOKER) >= 4:
+            # In case of 4 or 5 jokers, hand A will be 5 of a kind,
+            # and the loop will for sure end this iteration.
+            hand_a_is_this_hand = True
+        else:
+            if not hand_a_permutations:
+                hand_a_permutations = generate_all_permutations(hand_a)
+            hand_a_is_this_hand = any(
+                [is_strongest_hand(hand) for hand in hand_a_permutations]
+            )
+
+        if hand_b.count(JOKER) >= 4:
+            hand_b_is_this_hand = True
+        else:
+            if not hand_b_permutations:
+                hand_b_permutations = generate_all_permutations(hand_b)
+            hand_b_is_this_hand = any(
+                [is_strongest_hand(hand) for hand in hand_b_permutations]
+            )
         if hand_a_is_this_hand and not hand_b_is_this_hand:
             return True
         elif not hand_a_is_this_hand and hand_b_is_this_hand:
@@ -119,6 +172,19 @@ def hand_a_stronger_than_hand_b(hand_a: list[int], hand_b: list[int]) -> bool:
 assert hand_a_stronger_than_hand_b([1, 1, 1, 1, 1], [1, 2, 5, 2, 8]) == True
 assert hand_a_stronger_than_hand_b([2, 2, 2, 2, 2], [1, 1, 1, 1, 1]) == True
 assert hand_a_stronger_than_hand_b([3, 3, 3, 8, 8], [1, 1, 1, 1, 5]) == False
+
+
+assert hand_a_stronger_than_hand_b(get_cards("KK677"), get_cards("32T3K"))
+assert hand_a_stronger_than_hand_b(get_cards("T55J5"), get_cards("32T3K"))
+assert hand_a_stronger_than_hand_b(get_cards("QQQJA"), get_cards("32T3K"))
+assert hand_a_stronger_than_hand_b(get_cards("KTJJT"), get_cards("32T3K"))
+
+assert hand_a_stronger_than_hand_b(get_cards("T55J5"), get_cards("KK677"))
+assert hand_a_stronger_than_hand_b(get_cards("QQQJA"), get_cards("KK677"))
+assert hand_a_stronger_than_hand_b(get_cards("KTJJT"), get_cards("KK677"))
+
+assert hand_a_stronger_than_hand_b(get_cards("QQQJA"), get_cards("T55J5"))
+assert hand_a_stronger_than_hand_b(get_cards("KTJJT"), get_cards("T55J5"))
 
 
 @dataclass
